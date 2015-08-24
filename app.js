b26a021accb6d7fd145cd2f0567b91f659d938eb
Anna Casper <anna.casper3@gmail.com>
2015-08-24 14:56:05 -0600

    begin camera shop

--- app.js
+++ app.js
@@ -6,5 +6,10 @@ app.controller("MyFirstController", function($scope){
 app.controller("ExercisesController", function($scope){
     $scope.FavColor = "Purple";
     $scope.secondsInACentury = (((24 * 60) * 60) * 365) * 100;
-    $scope.rightNow = new Date
+    $scope.rightNow = new Date;
+    $scope.names = ["Harry", "Ron", "Hermione", "Sirius", "Hedwig", "Tonks"];
+    $scope.symbols = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+    $scope.numbers = [1,1,2,5,6,9,9,9];
+    $scope.objects = {first: "Anna", last: "Casper", age: 25}
+
 })
