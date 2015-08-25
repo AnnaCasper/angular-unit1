3498a54856e3159ea3a05824999769bdba47963d
Anna Casper <anna.casper3@gmail.com>
2015-08-24 19:35:51 -0600

    section 6 complete

---
+++ contacts.js
@@ -0,0 +1,12 @@
+var app = angular.module('contactsApp', []);
+
+app.controller('ContactsAppController', function ($scope) {
+  $scope.test = 'test';
+  $scope.contacts = [{name: 'Anna', email: 'acasper@gmail.com', phone: '720-220-5651'}];
+  $scope.addContact = function (name, email, phone) {
+    $scope.contacts.push({name: name, email: email, phone: phone})
+    $scope.name = '';
+    $scope.email = '';
+    $scope.phone = '';
+  };
+})
